a55a65c59cacdedf370863778e899f9be2a594eb
Yoshiiro <59020911+Yoshiiro07@users.noreply.github.com>
2023-05-29 14:50:28 -0300

    Update with start solution
    Java API Backend springboot, using java version 11

---
+++ src/main/java/com/testesys/HotdogSystem/HotdogSystem/HotdogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.testesys.HotdogSystem.HotdogSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotdogSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotdogSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testesys/HotdogSystem/HotdogSystem/HotdogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testesys.HotdogSystem.HotdogSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotdogSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
